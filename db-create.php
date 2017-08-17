c7e15a115aae5ab9a5d5e8f2729c537d418f84a4
yash <yashthakar1997@users.noreply.github.com>
2017-08-17 13:50:09 +0530

    changes to db-create

--- db-create.php
+++ db-create.php
@@ -1,5 +1,17 @@
 <?php
 
+   $host        = "host = ec2-184-72-230-93.compute-1.amazonaws.com";
+   $port        = "port = 5432";
+   $dbname      = "dbname = dcot5ea8vgmr1t";
+   $credentials = "user = nwktolobhorubg password=85b3da606efa12296f61a6139f968445b14631032d8a3990e2cccf76c2b11b60";
+
+   $db = pg_connect( "$host $port $dbname $credentials"  );
+   if(!$db) {
+      echo "Error : Unable to open database\n";
+   } else {
+      echo "Opened database successfully\n";
+   }
+   
    $sql =<<<EOF
       CREATE TABLE COMPANY
       (ID INT PRIMARY KEY     NOT NULL,
